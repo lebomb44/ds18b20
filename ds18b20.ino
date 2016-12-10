03a78dc51bf1daed35e964c44c99db1b7730f042
LeBomb <cambon.olivier@gmail.com>
2016-12-10 12:35:59 +0100

    First test

--- ds18b20.ino
+++ ds18b20.ino
@@ -3,82 +3,82 @@
 /* DS18S20 Temperature chip i/o */
 
 OneWire  ds(9);  // on pin 9
-#define MAX_DS1820_SENSORS 1
-byte addr[MAX_DS1820_SENSORS][8];
+uint8_t addr[8];
+
 void setup(void) 
 {
   Serial.begin(115200);
-  if (!ds.search(addr[0])) 
-  {
-    Serial.print("No more addresses.");
+  if(!ds.search(addr)) {
+    Serial.print("No device found !");
     ds.reset_search();
     delay(250);
     return;
   }
-  if ( !ds.search(addr[1])) 
-  {
-    Serial.println("No more addresses.");
-    ds.reset_search();
-    delay(250);
+  Serial.print("Reading:");
+  for(uint8_t i=0; i<8; i++) {
+    Serial.print(" ");
+    Serial.print(addr[i], HEX);
+  }
+  Serial.println("");
+  if(OneWire::crc8( addr, 7) == addr[7]) {
+    Serial.print("GOOD: CRC is valid ! : 0x"); Serial.println(addr[7], HEX);
+  }
+  else {
+    Serial.println("BAD: CRC is NOT valid!");
+    Serial.print("  Expected: 0x"); Serial.print(OneWire::crc8( addr, 7), HEX); Serial.println();
+    Serial.print("  Got: 0x"); Serial.print(addr[7], HEX); Serial.println();
     return;
+  }
+  if(0x28 != addr[0]) {
+      Serial.print("Device is not a DS18S20 family device.\n");
+      Serial.print("  Expected: 0x"); Serial.print("28"); Serial.println();
+      Serial.print("  Got: 0x"); Serial.print(addr[0], HEX); Serial.println();
+      return;
   }
 }
-int HighByte, LowByte, TReading, SignBit, Tc_100, Whole, Fract;
-char buf[20];
 
 void loop(void) 
 {
-  byte i, sensor;
-  byte present = 0;
-  byte data[12];
+  Serial.println("============= LOOP ==============");
 
-  for (sensor=0;sensor<MAX_DS1820_SENSORS;sensor++)
-  {
-    if ( OneWire::crc8( addr[sensor], 7) != addr[sensor][7]) 
-    {
-      Serial.println("CRC is not valid");
-      return;
-    }
+  ds.reset();
+  ds.select(addr);
+  Serial.println("Starting conversion");
+  ds.write(0x44,1);         // start conversion, with parasite power on at the end
 
-    if ( addr[sensor][0] != 0x28) 
-    {
-      Serial.print("Device is not a DS18B20 family device: ");
-      Serial.println(addr[sensor][0], HEX);
-      return;
-    }
+  delay(1000);     // maybe 750ms is enough, maybe not
+  // we might do a ds.depower() here, but the reset will take care of it.
 
-    ds.reset();
-    ds.select(addr[sensor]);
-    ds.write(0x44,1);         // start conversion, with parasite power on at the end
+  uint8_t present = ds.reset();
+  ds.select(addr);
+  Serial.println("Writing 0xBE");
+  ds.write(0xBE);         // Read Scratchpad
 
-    delay(1000);     // maybe 750ms is enough, maybe not
-    // we might do a ds.depower() here, but the reset will take care of it.
-
-    present = ds.reset();
-    ds.select(addr[sensor]);    
-    ds.write(0xBE);         // Read Scratchpad
-
-    for ( i = 0; i < 9; i++) 
-    {           // we need 9 bytes
-      data[i] = ds.read();
-    }
-
-    LowByte = data[0];
-    HighByte = data[1];
-    TReading = (HighByte << 8) + LowByte;
-    SignBit = TReading & 0x8000;  // test most sig bit
-    if (SignBit) // negative
-    {
-      TReading = (TReading ^ 0xffff) + 1; // 2's comp
-    }
-    Tc_100 = (TReading*100/2);    
-
-    Whole = Tc_100 / 100;  // separate off the whole and fractional portions
-    Fract = Tc_100 % 100;
-
-    sprintf(buf, "%d:%c%d.%d\337C     ",sensor,SignBit ? '-' : '+', Whole, Fract < 10 ? 0 : Fract);
-
-    Serial.println(buf);
+  Serial.print("Reading:");
+  uint8_t data[8] = {0};
+  for (uint8_t i = 0; i < 9; i++) {           // we need 9 bytes
+    data[i] = ds.read();
+    Serial.print(" ");
+    Serial.print(data[i], HEX);
   }
+  Serial.println("");
+
+  if(OneWire::crc8(data, 8) == data[8]) {
+    Serial.print("GOOD: CRC is valid ! : 0x"); Serial.println(data[8], HEX);
+  }
+  else {
+    Serial.println("BAD: CRC is NOT valid!");
+    Serial.print("  Expected: 0x"); Serial.print(OneWire::crc8(data, 8), HEX); Serial.println();
+    Serial.print("  Got: 0x"); Serial.print(data[8], HEX); Serial.println();
+    return;
+  }
+
+  uint16_t TReading = (((uint16_t) data[1]) << 8) + data[0];
+  //if(0x8000 & TReading) { // negative
+  //  TReading = (TReading ^ 0xffff) + 1; // 2's comp
+  //}
+  double temperature = (int16_t) TReading;
+  Serial.print("temperature: "); Serial.println(temperature/16.0);
+  delay(10000);
 }
 
